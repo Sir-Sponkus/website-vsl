ba494a5b580ebaece1409cb6cd9dead5bcbb631d
Sir-Sponkus <superfoxymlglol@gmail.com>
2026-07-30 00:06:25 +0700

    please work

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? 'website-vsl' : process.env.BASE_PATH
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html', 
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME || ''}` : ''
+    }
+  }
 };
 
 export default config;
